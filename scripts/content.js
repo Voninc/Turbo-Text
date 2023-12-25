6afeeb52e30c8761313697717c5f8507c101a41e
Waqar Amin <vacaramin86@gmail.com>
2023-12-26 04:32:59 +0500

    EXT working now, however turn off button doesn't work

--- scripts/content.js
+++ scripts/content.js
@@ -1,28 +1,51 @@
 function boldCharactersInWords(node) {
-    if (node.nodeType === Node.TEXT_NODE) {
+  if (node.nodeType === Node.TEXT_NODE) {
       const words = node.nodeValue.split(/\s+/);
       const modifiedWords = words.map((word) => {
-        if (word.length > 1) {
-          const charsToBold = Math.ceil(word.length * 0.35);
-          const boldedPart = `<strong>${word.slice(0, charsToBold)}</strong>`;
-          const remainingPart = word.slice(charsToBold);
-          return boldedPart + remainingPart;
-        }
-        return word;
+          if (word.length > 1) {
+              const charsToBold = Math.ceil(word.length * 0.35);
+              const boldedPart = `<strong>${word.slice(0, charsToBold)}</strong>`;
+              const remainingPart = word.slice(charsToBold);
+              return boldedPart + remainingPart;
+          }
+          return word;
       });
-  
+
       const modifiedText = modifiedWords.join(' ');
       const newElement = document.createElement('span');
       newElement.innerHTML = modifiedText;
-  
+
       // Replace the original text node with the modified HTML
       node.parentNode.replaceChild(newElement, node);
-    } else if (node.nodeType === Node.ELEMENT_NODE) {
+  } else if (node.nodeType === Node.ELEMENT_NODE) {
       // Recursively process child nodes for element nodes
       node.childNodes.forEach((childNode) => boldCharactersInWords(childNode));
-    }
   }
-  
-  // Apply bolding to the entire document body
-  boldCharactersInWords(document.body);
-  
+}
+
+// Function to remove bolding from the entire document body
+function removeBolding(node) {
+  if (node.nodeType === Node.ELEMENT_NODE) {
+      node.querySelectorAll('strong').forEach((strongElement) => {
+          const textNode = document.createTextNode(strongElement.textContent);
+          strongElement.parentNode.replaceChild(textNode, strongElement);
+      });
+  }
+}
+
+// Initial bolding on page load
+boldCharactersInWords(document.body);
+
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'toggle') {
+      // Toggle the extension on/off
+      if (request.isEnabled) {
+          // If enabled, remove bolding
+          removeBolding(document.body);
+      } else {
+          // If disabled, apply bolding
+          boldCharactersInWords(document.body);
+      }
+  }
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,24 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('toggleButton');
-  
-    chrome.storage.sync.get('isEnabled', function (data) {
+  const toggleButton = document.getElementById('toggleButton');
+
+  chrome.storage.sync.get('isEnabled', function (data) {
       const isEnabled = data.isEnabled || false;
-  
+
       if (isEnabled) {
-        toggleButton.textContent = 'Turn Off';
+          toggleButton.textContent = 'Off';
+          toggleButton.classList.add('off');
       } else {
-        toggleButton.textContent = 'Turn On';
+          toggleButton.textContent = 'On';
       }
-    });
-  
-    toggleButton.addEventListener('click', function () {
-      chrome.runtime.sendMessage({ action: 'toggle' }, function (response) {
-        if (response.isEnabled) {
-          toggleButton.textContent = 'Turn Off';
-        } else {
-          toggleButton.textContent = 'Turn On';
-        }
-      });
-    });
   });
-  
+
+  toggleButton.addEventListener('click', function () {
+      chrome.runtime.sendMessage({ action: 'toggle' }, function (response) {
+          const isCurrentlyEnabled = toggleButton.classList.contains('off');
+
+          if (isCurrentlyEnabled) {
+              toggleButton.textContent = 'On';
+              toggleButton.classList.remove('off');
+          } else {
+              toggleButton.textContent = 'Off';
+              toggleButton.classList.add('off');
+          }
+      });
+  });
+});
